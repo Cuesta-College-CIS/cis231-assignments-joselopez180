cbbd3635c86d1f5c7de97f793ac3d5d1e27313ee
Jose Lopez <jose_lopez21@my.cuesta.edu>
2022-02-11 13:08:48 -0800

    Final Product 1:08pm

--- GreatestNumber.java
+++ GreatestNumber.java
@@ -1,32 +1,46 @@
+/*
+    Program Name: GreatestNumber.java
+    Author: Jose Antonio Lopez
+    Date: 02/11/2022
+    Program Description (brief): 
+        In this code the objective is to generate three random 
+    numbers from 0-100 and inform the user what thoes numbers 
+    are. From there, the code will identify what number is 
+    greatest out of the three random numbers and inform the user 
+    what it is.
+*/
+
 import java.util.Random;
 
 public class GreatestNumber {
 
     public static void main( String args[] ) {
-        Random rand = new Random(); //instance of random class
+        //instance of random class
+        Random rand = new Random();
         
 
-        //generate random values from 0-100
+        //generate random values from 0-100.
         int num1 = rand.nextInt(101); 
         int num2 = rand.nextInt(101);
         int num3 = rand.nextInt(101);
 
+        //Informing the user of the three random numbers.
         System.out.println("Random numbers are: " + num1 + " " + num2 + " "+ num3);
 
+        //If num1 is greater then out it.
         if( (num1 > num2) && (num1 > num3) ){
             System.out.println("The greatest number is: " + num1);
         }
-
+        //If num2 is greater then out it.
         else if( (num2 > num1) && (num2 > num3) ){
             System.out.println("The greatest number is: " + num2);
         }
-
+        //If num3 is greater then out it.
         else{
             System.out.println("The greatest number is: " + num3);
         }
 
-
-
+        // END OF CODE
 
       }   
 }
