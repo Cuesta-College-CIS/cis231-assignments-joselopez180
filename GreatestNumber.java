71f2ac89ac7d1db7efea89c1141790ac1be231a2
Jose Lopez <jose_lopez21@my.cuesta.edu>
2022-02-11 12:55:53 -0800

    improvment

--- GreatestNumber.java
+++ GreatestNumber.java
@@ -1,14 +1,32 @@
+import java.util.Random;
+
 public class GreatestNumber {
-    
-int num1;
-int num2;
-int num3;
+
+    public static void main( String args[] ) {
+        Random rand = new Random(); //instance of random class
+        
+
+        //generate random values from 0-100
+        int num1 = rand.nextInt(101); 
+        int num2 = rand.nextInt(101);
+        int num3 = rand.nextInt(101);
+
+        System.out.println("Random numbers are: " + num1 + " " + num2 + " "+ num3);
+
+        if( (num1 > num2) && (num1 > num3) ){
+            System.out.println("The greatest number is: " + num1);
+        }
+
+        else if( (num2 > num1) && (num2 > num3) ){
+            System.out.println("The greatest number is: " + num2);
+        }
+
+        else{
+            System.out.println("The greatest number is: " + num3);
+        }
 
 
-System.out.print(java.util.Random);
 
 
-
-
-
+      }   
 }
