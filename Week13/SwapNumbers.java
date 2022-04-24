aa76093b681d1452108f856e318475bfb238532b
joselopez180 <jose_lopez21@my.cuesta.edu>
2022-04-24 01:55:08 -0700

    This works 10/10

---
+++ Week13/SwapNumbers.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+ class Number {
+    private int num;
+    
+    public Number(int n) {
+       num = n;
+    }
+    
+    public int getNum() {
+       return num;
+    }
+    
+    public void setNum(int n) {
+       num = n;
+    }
+ }
+
+
+public class SwapNumbers {
+   
+   public static void swap(Number num1, Number num2) {
+      /* Your code goes here */
+        int temp = num1.getNum();
+        num1.setNum(num2.getNum());
+        num2.setNum(temp);
+   }
+   
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int int1;
+      int int2;
+      
+      int1 = scnr.nextInt();
+      int2 = scnr.nextInt();
+      
+      Number num1 = new Number (int1);
+      Number num2 = new Number (int2);
+      
+      swap(num1, num2);
+      System.out.println("num1 = " + num1.getNum() + ", num2 = " + num2.getNum());
+      scnr.close();
+   }
+}
